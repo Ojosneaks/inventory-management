409d5988b2221bc56e67c86980ce418efa1ed922
Timi Ojo (SPS-INT) <sps.bky.timio@nucor.com>
2025-06-16 16:54:26 -0400

    first commit

---
+++ src/app/(components)/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const Navbar = () => {
+  return (
+    <div className='flex-justify-between items-center w-full mb-7'>
+      Navbar
+      </div>
+  );
+};
+
+export default Navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter} from "next/font/google";
 import "./globals.css";
+import DashboardWrapper from "@/app/dashboard/dashboardWrapper";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -25,9 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={inter.className}>
+        <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
   );
